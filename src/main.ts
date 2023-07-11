f1eb52f1e1c9bc5a38c4e3116cc284b6069752c9
andreyalbuquerquee <112346889+andreyalbuquerquee@users.noreply.github.com>
2023-07-11 07:08:26 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.useGlobalPipes(new ValidationPipe);
-  
+  app.enableCors({ origin: '*' });
   
   await app.listen(3000);
 }
